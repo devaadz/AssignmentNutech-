1cac2a18b3b6e1bc999c05c2c3a3f1381e50f055
devaadz <devawaow@gmail.com>
2025-01-26 00:27:06 +0700

    commit SECOND

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
 
