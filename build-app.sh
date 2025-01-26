a876aa9dee8fb90c7cccc43a8ccfbff385e178dc
devaadz <devawaow@gmail.com>
2025-01-26 22:15:54 +0700

    railway

--- build-app.sh
+++ build-app.sh
@@ -8,10 +8,10 @@ set -e
 npm run build
 
 # Clear cache
-# php artisan optimize:clear
+php artisan optimize:clear
 
 # Cache the various components of the Laravel application
-# php artisan config:cache
-# php artisan event:cache
-# php artisan route:cache
-# php artisan view:cache
+php artisan config:cache
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
